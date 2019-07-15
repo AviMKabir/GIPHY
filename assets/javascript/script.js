23249fc24e16773067dca089f13d68192e0d637e
Avi M Kabir <avimishkakabir@gmail.com>
2019-07-15 12:07:30 -0400

    all starter code works now

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,102 +1,110 @@
 
 
-      var movies = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
+var movies = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
 
 
-      renderButtons();
+
+function displayMovieInfo() {
 
 
-$("button").on("click", function() {
-      // In this case, the "this" keyword refers to the button that was clicked
-      var person = $(this).attr("data-name");
-      var apiKey = "rVACN8PXJGdFJVbnO8UQrFW3sJ6WDwAA";
-      var limit = 13;
 
-      // Constructing a URL to search Giphy for the name of the person who said the quote
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        person + "&api_key=" + apiKey + "&limit=" + limit;
+        // In this case, the "this" keyword refers to the button that was clicked
+        var person = $(this).attr("data-name");
+        var apiKey = "rVACN8PXJGdFJVbnO8UQrFW3sJ6WDwAA";
+        var limit = 13;
 
-      // Performing our AJAX GET request
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-        // After the data comes back from the API
-        .then(function(response) {
+        // Constructing a URL to search Giphy for the name of the person who said the quote
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            person + "&api_key=" + apiKey + "&limit=" + limit;
 
-            $(".gifDiv").remove();
-          // Storing an array of results in the results variable
-          var results = response.data;
+        // Performing our AJAX GET request
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            // After the data comes back from the API
+            .then(function (response) {
 
-          // Looping over every result item
-          for (var i = 0; i < results.length; i++) {
+                $(".gifDiv").remove();
+                // Storing an array of results in the results variable
+                var results = response.data;
 
-            // Only taking action if the photo has an appropriate rating
-            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-              // Creating a div for the gif
-              var gifDiv = $("<div class = 'gifDiv' >");
+                // Looping over every result item
+                for (var i = 0; i < results.length; i++) {
 
-              // Storing the result item's rating
-              var rating = results[i].rating;
+                    // Only taking action if the photo has an appropriate rating
+                    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                        // Creating a div for the gif
+                        var gifDiv = $("<div class = 'gifDiv' >");
 
-              // Creating a paragraph tag with the result item's rating
-              var p = $("<p>").text("Rating: " + rating);
+                        // Storing the result item's rating
+                        var rating = results[i].rating;
 
-              // Creating an image tag
-              var personImage = $("<img>");
+                        // Creating a paragraph tag with the result item's rating
+                        var p = $("<p>").text("Rating: " + rating);
 
-              // Giving the image tag an src attribute of a proprty pulled off the
-              // result item
-              personImage.attr("src", results[i].images.fixed_height.url);
+                        // Creating an image tag
+                        var personImage = $("<img>");
 
-              // Appending the paragraph and personImage we created to the "gifDiv" div we created
-              gifDiv.append(p);
-              gifDiv.append(personImage);
+                        // Giving the image tag an src attribute of a proprty pulled off the
+                        // result item
+                        personImage.attr("src", results[i].images.fixed_height.url);
 
-              // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-              $("#gifs-appear-here").prepend(gifDiv);
-            }
-          }
-        });
-    });
+                        // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                        gifDiv.append(p);
+                        gifDiv.append(personImage);
 
-    function renderButtons() {
+                        // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                        $("#gifs-appear-here").prepend(gifDiv);
+                    }
+                }
+            });
+    
 
-        // Deleting the movies prior to adding new movies
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#buttons-view").empty();
 
-        // Looping through the array of movies
-        for (var i = 0; i < movies.length; i++) {
 
-          // Then dynamicaly generating buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class of movie-btn to our button
-          a.addClass("movie-btn");
-          // Adding a data-attribute
-          a.attr("data-name", movies[i]);
-          // Providing the initial button text
-          a.text(movies[i]);
-          // Adding the button to the buttons-view div
-          $("#buttons-view").append(a);
-        }
-      }
+}
 
-      // This function handles events where a movie button is clicked
-      $("#add-movie").on("click", function(event) {
-        event.preventDefault();
-        // This line grabs the input from the textbox
-        var movie = $("#movie-input").val().trim();
 
-        // Adding movie from the textbox to our array
-        movies.push(movie);
+  
+function renderButtons() {
 
-        // Calling renderButtons which handles the processing of our movie array
-        renderButtons();
-      });
+    // Deleting the movies prior to adding new movies
+    // (this is necessary otherwise you will have repeat buttons)
+    $("#buttons-view").empty();
 
-      // Adding a click event listener to all elements with a class of "movie-btn"
-      $(document).on("click", ".movie-btn", displayMovieInfo);
+    // Looping through the array of movies
+    for (var i = 0; i < movies.length; i++) {
 
-      renderButtons();
+      // Then dynamicaly generating buttons for each movie in the array
+      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+      var a = $("<button>");
+      // Adding a class of movie-btn to our button
+      a.addClass("movie-btn");
+      // Adding a data-attribute
+      a.attr("data-name", movies[i]);
+      // Providing the initial button text
+      a.text(movies[i]);
+      // Adding the button to the buttons-view div
+      $("#buttons-view").append(a);
+    }
+  }
+
+  // This function handles events where a movie button is clicked
+  $("#add-movie").on("click", function(event) {
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var movie = $("#movie-input").val().trim();
+
+    // Adding movie from the textbox to our array
+    movies.push(movie);
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+  });
+
+  // Adding a click event listener to all elements with a class of "movie-btn"
+  $(document).on("click", ".movie-btn", displayMovieInfo);
+
+  // Calling the renderButtons function to display the intial buttons
+  renderButtons();
